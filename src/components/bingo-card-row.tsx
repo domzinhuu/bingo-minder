3bbebaac167d670c50a91e5414c34d2ad8587ba5
Lucas Kindinger <kindingerlek@hotmail.com>
2024-09-10 11:36:05 -0300

    refact: simplifies the parameters to `bingo-card-row` component

--- src/components/bingo-card-row.tsx
+++ src/components/bingo-card-row.tsx
@@ -2,17 +2,14 @@ import { BingoRowItem } from "./bingo-row-item";
 import { getCheckedNumbers } from "@/lib/utils";
 
 interface Props {
-  cardNumbers: Record<string, number[]>;
-  rowIndex: number;
+  rowValues: Array<number>;
 }
-export function BingoCardRow({ cardNumbers, rowIndex }: Props) {
+export function BingoCardRow({ rowValues }: Props) {
   const checkedNumbers: number[] = getCheckedNumbers();
 
   const verifyIfIsAlreadyChecked = (value: number) => {
     return checkedNumbers.includes(value);
   };
-
-  const rowValues = Object.keys(cardNumbers).map((letter) => cardNumbers[letter][rowIndex]);
 
   return (
     <div className="w-full flex items-center gap-2 text-slate-900">
--- src/components/bingo-card.tsx
+++ src/components/bingo-card.tsx
@@ -4,11 +4,13 @@ interface Props {
   cardNumbers: Record<string, number[]>;
 }
 export function BingoCard({ cardNumbers }: Props) {
+  /** Convert and transpose the object cardNumber to an array of arrays, with each Letter of BINGO as column */
+  const rows = [...Array<number>(5).keys()].map((i) => Object.keys(cardNumbers).map((k) => cardNumbers[k][i]));
   return (
     <div className="rounded-lg space-y-2">
       <BingoHeaderRow />
-      {[...Array<number>(5).keys()].map((index) => (
-        <BingoCardRow key={"row" + index} cardNumbers={cardNumbers} rowIndex={index}/>
+      {rows.map((rowValues, index) => (
+        <BingoCardRow key={"row_" + index} rowValues={rowValues} />
       ))}
     </div>
   );
